29c1309c6da862c2563a605ad74ae439d09c733e
mimistancheva <44364600+mimistancheva@users.noreply.github.com>
2018-11-24 14:06:29 +0200

    Add files via upload

---
+++ Capter 6/Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < length; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Capter 6/Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < length; i++)
+            {
+                if (i % (3 * 7) != 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
