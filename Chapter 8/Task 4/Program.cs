93db1dd6bcef59eca1910319bbac07fe436df5fc
mimistancheva <44364600+mimistancheva@users.noreply.github.com>
2018-11-26 13:48:43 +0200

    Add files via upload

---
+++ Chapter 8/Task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+    int deci = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("{0} to binary is {1}.",
+        arg0: deci, arg1: deci.ToString(deci, 2));
+        }
+    }
+}
