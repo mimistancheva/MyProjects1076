b77a837d4d2dbf4284442fc291c3fd828efeb83f
mimistancheva <44364600+mimistancheva@users.noreply.github.com>
2018-11-26 13:50:10 +0200

    Add files via upload

---
+++ Chapter 9/Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void ReturnName(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+        }
+    }
+}
